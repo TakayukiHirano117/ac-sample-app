571e0ade0889729054ca7aede51bcb04460b95c8
TakayukiHirnao117 <ikusa.hirano@icloud.com>
2024-12-12 12:57:10 +0900

    ♻️ delete用のroute.tsでParams型を定義

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,9 +1,15 @@
 import { NextRequest } from "next/server";
 import { supabase } from "../../../../../utils/supabase";
 
+interface Params {
+  params: {
+    id: string; // paramsはstring型が期待される
+  };
+}
+
 export const DELETE = async (
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: Params
 ) => {
   const id = parseInt(params.id);
   const { error } = await supabase.from("todos").delete().eq("id", id);
