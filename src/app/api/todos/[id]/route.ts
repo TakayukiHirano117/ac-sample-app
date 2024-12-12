592a485d9a896ae7ed99901110f9baee0f39b609
TakayukiHirnao117 <ikusa.hirano@icloud.com>
2024-12-12 12:59:19 +0900

    🐛 route.tsのPromise型への対応

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,17 +1,12 @@
 import { NextRequest } from "next/server";
 import { supabase } from "../../../../../utils/supabase";
 
-interface Params {
-  params: {
-    id: string; // paramsはstring型が期待される
-  };
-}
 
 export const DELETE = async (
   req: NextRequest,
-  { params }: Params
+  { params }: { params: Promise<{ id: string }> }
 ) => {
-  const id = parseInt(params.id);
+  const id = parseInt((await params).id);
   const { error } = await supabase.from("todos").delete().eq("id", id);
 
   if (error) {
