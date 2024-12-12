ca33759bc10c040f9dde3d34379652ab7373c046
TakayukiHirnao117 <ikusa.hirano@icloud.com>
2024-12-12 16:00:05 +0900

    ♻️ POSTのレスポンスを変更

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,14 +1,14 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { supabase } from "../../../../utils/supabase";
 
 export const GET = async () => {
   const { data: todos, error } = await supabase.from("todos").select("*");
 
   if (error) {
-    return Response.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 
-  return Response.json(todos, { status: 200 });
+  return NextResponse.json(todos, { status: 200 });
 };
 
 export const POST = async (req: NextRequest) => {
@@ -17,9 +17,9 @@ export const POST = async (req: NextRequest) => {
   const { data, error } = await supabase.from("todos").insert({ title: title });
 
   if (error) {
-    return Response.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 
-  return Response.json(data, { status: 201 });
+  return NextResponse.json(data, { status: 201 });
 };
 
